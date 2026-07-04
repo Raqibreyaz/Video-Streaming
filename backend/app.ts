2031f9bf9dab0a26d1fcaf327a7c52d61b02fb1f
Raquib Reyaz <raquibreyaz111@gmail.com>
2026-07-05 02:27:04 +0530

    trimmed white spaces from env vars for better flexibility

--- backend/app.ts
+++ backend/app.ts
@@ -6,7 +6,13 @@ const ecsClient = new ECSClient({
   region: process.env["AWS_REGION"] ?? "ap-south-1",
 });
 
-export const lamda = async (event: S3Event) => {
+const taskDefinitionARN = process.env["TASK_DEFINITION_ARN"]?.trim()!;
+const clusterARN = process.env["CLUSTER_ARN"]?.trim()!;
+const subnetIDs = process.env["SUBNET_IDS"]?.trim().split(",")!;
+const securityGroupId = process.env["SECURITY_GROUP_ID"]?.trim()!;
+const outputBucket = process.env["OUTPUT_BUCKET"]?.trim();
+
+export const lambda = async (event: S3Event) => {
   const objectKey = decodeURIComponent(
     event.Records[0]?.s3?.object?.key?.replace(/\+/g, " ") || "",
   );
@@ -19,13 +25,13 @@ export const lamda = async (event: S3Event) => {
   }
 
   const runTaskCommand = new RunTaskCommand({
-    taskDefinition: process.env["TASK_DEFINITION_ARN"],
-    cluster: process.env["CLUSTER_ARN"],
+    taskDefinition: taskDefinitionARN,
+    cluster: clusterARN,
     launchType: "FARGATE",
     networkConfiguration: {
       awsvpcConfiguration: {
-        subnets: process.env["SUBNET_IDS"]?.split(","),
-        securityGroups: [process.env["SECURITY_GROUP_ID"]!],
+        subnets: subnetIDs,
+        securityGroups: [securityGroupId],
         assignPublicIp: "ENABLED",
       },
     },
@@ -37,7 +43,7 @@ export const lamda = async (event: S3Event) => {
             { name: "JOB_ID", value: crypto.randomUUID() },
             { name: "INPUT_BUCKET", value: tempBucketName },
             { name: "INPUT_KEY", value: objectKey },
-            { name: "OUTPUT_BUCKET", value: process.env["OUTPUT_BUCKET"] },
+            { name: "OUTPUT_BUCKET", value: outputBucket },
           ],
         },
       ],
